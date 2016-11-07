e0cebca2e0f9f21d340d93b072b5af32e6700ac2
Jose Garita Chaves <garitaj@gmail.com>
2016-11-06 18:12:55 -0600

    Revert "Revert "ventanas necesarias arregladas y selecionadas para la primera entrega""
    This reverts commit 312471ab1ba996db1e2fb5155c2afd64cb520933.

---
+++ proyectoPolaco/src/Modelo/Client.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+public class Client {
+    private String location;
+    private String name;
+    private String lastName;
+    private String phone;
+    private String mail;
+    
+}
